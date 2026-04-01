e938f551fc2262b571521e4e81cbca58a48cfe31
Nishcahy <nishchaynilekani@gmail.com>
2026-04-01 12:22:46 +0530

    made identity service working for login

--- identity-service/src/main/java/com/nishchay/identity_service/service/JwtService.java
+++
@@ -1,9 +0,0 @@
-package com.nishchay.identity_service.service;
-
-import org.springframework.security.core.Authentication;
-
-public interface JwtService {
-
-    void validateToken(final String token);
-    String generateToken(Authentication authentication);
-}
--- identity-service/src/main/java/com/nishchay/identity_service/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.nishchay.identity_service.service;
-
-import com.nishchay.identity_service.dto.UserDto;
-
-public interface UserService {
-    UserDto getUserByUsername(String userName);
-}
