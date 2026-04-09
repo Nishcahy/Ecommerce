8cd2bb934b8a4a6a9fe5dc80164080cb54daf5fc
nishchay <nishchaynilekani@gmail.com>
2026-04-09 17:37:49 +0530

    made the email service and feing to order

--- email-service/src/main/java/com/nishchay/email_service/service/ProductApiClient.java
+++ email-service/src/main/java/com/nishchay/email_service/service/ProductApiClient.java
@@ -1,5 +1,16 @@
 package com.nishchay.email_service.service;
 
-public class ProductApiClient {
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.nishchay.commonlib.dto.ApiResponce;
+import com.nishchay.email_service.dto.ProductStockResponseDto;
+
+@FeignClient(name="PRODUCT-SERVICE")
+public interface ProductApiClient {
+	
+	@GetMapping("api/v1/products/{id}")
+	ResponseEntity<ApiResponce<ProductStockResponseDto>> getProductById(@RequestParam("id") String id);
 }
